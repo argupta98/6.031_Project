1c869d1ed50e939f2bce90852dc16ec929198318
dotunadegbite <dotunadegbite23@gmail.com>
2018-05-02 12:23:38 -0400

    Changed Chord to take List of Music

--- src/karaoke/lyrics/Lyrics.java
+++ src/karaoke/lyrics/Lyrics.java
@@ -1,7 +1,7 @@
 package karaoke.lyrics;
 
 
-public class Lyrics {
+public interface Lyrics {
     //Datatype definition
     // Lyric = Break(first: Lyric, second: Lyric) + Syllable(value: String) + Skipped() 
     //         + CombineWords(first: Lyric, second: Lyric)+combineSyllables(first: Lyric, second: Lyric)
