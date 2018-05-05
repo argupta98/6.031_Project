02e359e1dc652d7bc126840ab6eb63b45507ac4c
argupta98 <argupta@mit.edu>
2018-05-05 10:57:13 -0400

    filled in test cases for ParseAST

--- src/karaoke/Composition.java
+++ src/karaoke/Composition.java
@@ -1,12 +1,16 @@
 package karaoke;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
+import karaoke.Composition.Key;
 import karaoke.Voice.LyricListener;
 
 public class Composition {
     private final Map<String, Voice> voices;
+    public enum Key{A, B, C, D, E, F, G}
     
     //AF(Voices) = A composition music piece which consists of all the voices in voices played
     //             together
@@ -42,4 +46,59 @@ public class Composition {
         throw new UnsupportedOperationException("not implemented yet");
     }
     
+    /**
+     * @return the duration of the entire Composition
+     */
+    public double duration() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    /**
+     * @return the title of the composition
+     */
+    public String title() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    /**
+     * @return the composer of the composition
+     */
+    public String composer() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    /**
+     * @return the tempo of the composition
+     */
+    public double tempo() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    /**
+     * @return the default length of a note in the composition
+     */
+    public double length() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    /**
+     * @return the track number of the composition in a set of compositions
+     */
+    public int trackNumber() {
+        throw new UnsupportedOperationException("not implemented yet"); 
+    }
+    
+    /**
+     * @return the key the composition is written in
+     */
+    public Key key(){
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    /**
+     * @return the length of a bar of a composition
+     */
+    public double meter() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
 }
